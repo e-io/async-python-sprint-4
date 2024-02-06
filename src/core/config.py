d9650acf55b5d4c99402237f5f1c1b3c36c0d3a2
Pavel 宝尔米 <ur.ay@ya.ru>
2024-02-06 14:13:22 +0200

    Tidy Up 1 (#3)
    readme is updated with an instruction how to start

--- src/core/config.py
+++ src/core/config.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from pydantic import BaseSettings
 
 BASE_DIR = Path.cwd().parent.parent
-print('BASE_DIR', BASE_DIR)
+# print('BASE_DIR', BASE_DIR)
 
 
 class AppSettings(BaseSettings):
-    PROJECT_NAME: str = 'Default name'
+    PROJECT_NAME: str  # set any name in '.env' file
 
     class Config:
         env_file = '.env'
